3f4b759ed263dee6cf9d6b652da3147e4aeec71d
madipavan <amodedipesh12@gmail.com>
2024-01-15 02:08:58 +0530

    updated

---
+++ script.js
@@ -0,0 +1,7 @@
+function menu()
+{
+    var innav = document.getElementById("innav");
+    var ico = document.getElementById("ico");
+    ico.classList.toggle("open");
+    innav.classList.toggle("innav-open")
+}
